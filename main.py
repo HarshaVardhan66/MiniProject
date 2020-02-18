5c9eea0f2cb5d27068efcb4fdee727fc47baa344
Harsha Vardhan Reddy Mamidi <46490922+HarshaVardhan66@users.noreply.github.com>
2020-02-18 22:20:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 from pandas import *
-import os
 from flask import *
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
 
 app = Flask('__main__')
+
+
+def linearRegression(fit_data, test_data):
+    reg = linear_model.LinearRegression()
+    reg.fit(fit_data[['Date']], fit_data.Temp)
+    value = float(reg.predict([[int(request.form['year'])]]))
+    return value
 
 
 @app.route('/')
@@ -69,6 +78,62 @@ def showResult():
 @app.route('/predict')
 def predict():
     return render_template('predict.html')
+
+
+@app.route('/predict_temp', methods=['POST'])
+def predict_temp():
+    # Getting the required Data
+    data = pd.read_csv('D:\Harsha\Python\MiniProject\Data\Cities-Filtered.csv')
+    name = request.form['city']
+    name = name.capitalize()
+    if name == 'Chennai':
+        name = 'Madras'
+    if name == 'Mumbai':
+        name = 'Bombay'
+    city = [name]
+    if name not in set(data.City):
+        return "<h1><Center>No such city Found"
+    data = data[data['City'].isin(city)]
+    data = data[['dt', 'AverageTemperature']]
+    data = data.reset_index(drop=True)
+    data = data.rename(columns={'dt': 'Date', 'AverageTemperature': 'Temp'})
+
+    dates = list(set(data['Date']))
+    dates.sort()
+    temps = list(data['Temp'])
+
+    # Extracting Fitting data
+    start = '1900'
+    end = '1950'
+    year = int(start)
+    data_dict = {'Date': [], 'Temp': []}
+    for i in range(dates.index(start + '-01-01'), dates.index(end + '-12-01'), 12):
+        add = 0
+        for j in range(0, 12):
+            add = add + float(temps[i + j])
+        add = add / 12
+        data_dict['Date'].append(year)
+        data_dict['Temp'].append(add)
+        year = year + 1
+    fit_data = pd.DataFrame.from_dict(data_dict)
+
+    # Extracting Testing data
+    start = '1951'
+    end = '2012'
+    year = int(start)
+    data_dict = {'Date': [], 'Temp': []}
+    for i in range(dates.index(start + '-01-01'), dates.index(end + '-12-01'), 12):
+        add = 0
+        for j in range(0, 12):
+            add = add + float(temps[i + j])
+        add = add / 12
+        data_dict['Date'].append(year)
+        data_dict['Temp'].append(add)
+        year = year + 1
+    test_data = pd.DataFrame.from_dict(data_dict)
+
+    value = linearRegression(fit_data, test_data)
+    return "<center>Value is : " + str(value)[:5]
 
 
 if __name__ == '__main__':
